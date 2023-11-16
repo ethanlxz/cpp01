022e50628058aaedb497681a5d5f01266e6de2f3
ethanlxz <ethanlxz@gmail.com>
2023-11-16 22:12:03 +0800

    added makefile

--- ex04/Main.cpp
+++ ex04/Main.cpp
@@ -3,5 +3,10 @@
 
 int main(int ac, char **av)
 {
-	if (ac != 4 ||)
+	if (ac != 4 || !av[2][0] || !av[3][0] )
+	{
+        std::cout << "Error ! Use ./replace file s1 s2." << std::endl;
+		return 0;
+	}
+
 }
